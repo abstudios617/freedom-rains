cb59558dc7f4e26e9f6d20a9685756d418730557
MasonBoom <masonboom@gmail.com>
2022-08-25 12:55:19 -0400

    Completed product item and checkout

--- client/src/pages/cart/cart.styles.js
+++ client/src/pages/cart/cart.styles.js
@@ -36,9 +36,187 @@ export const CartOptions = styled.div`
     text-decoration-line: underline;
     color: #828A94;
     margin-left: 30px;
+    cursor: pointer;
 
     &.first {
       margin-left: 0px;
     }
   }
+`;
+
+export const ProductColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ProductImg = styled.img`
+  width: 141px;
+  height: 141px;
+  border: 1px solid #DEE1E5;
+`;
+
+export const ButtonRow = styled.div`
+  display: flex-;
+  flex-direction: row;
+  margin-top: 18px;
+  margin-bottom: 28px;
+`;
+
+export const CouponButton = styled.button`
+  margin-left: 21px;
+  width: 99px;
+  height: 32px;
+  background: #1988FF;
+  border: 1px solid #1988FF;
+  border-radius: 6px;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 18px;
+  color: #FFFFFF;
+  cursor: pointer;
+`;
+
+export const SaveForLaterButton = styled.div`
+  width: 156px;
+  height: 32px;
+  border: 1px solid #1988FF;
+  border-radius: 6px;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 18px;
+  color: #1988FF;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  margin-left: 21px;
+`;
+
+export const ProductList = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 815px;
+  margin-top: 48px;
+`;
+
+export const Product = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const ProductName = styled.span`
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 22px;
+  color: #3C4044;
+  margin-left: 11px;
+`;
+
+export const ProductDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: -300px;
+`;
+
+export const ExtraDetails = styled.ul`
+  list-style-type: none;
+  margin-left: -28px;
+
+  li {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 10px;
+    line-height: 12px;
+    color: #828282;
+  }
+`;
+
+export const Delete = styled.img`
+  cursor: pointer;
+  margin-top: 0px;
+  width: 16px;
+  height: 18px;
+  margin-left: 400px;
+  margin-top: -20px;
+`;
+
+export const SubtotalDiv = styled.div`
+  width: 300px;
+  height: 82px;
+  background: #1988FF;
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+  margin-left: -200px;
+
+  span {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 12px;
+    line-height: 20px;
+    margin-left: 31px;
+    margin-top: 5px;
+    color: #FFFFFF;
+  }
+`;
+
+export const Total = styled.h3`
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 30px;
+  line-height: 37px;
+  color: #FFFFFF;
+  float: right;
+  margin-left: auto;
+  margin-right: 19px;
+  margin-top: 10px;
+`;
+
+export const Checkout = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CheckoutButton = styled.button`
+  width: 300px;
+  height: 58px;
+  background: #8EC90E;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 10px;
+  margin-top: -15px;
+  color: #FFFFFF;
+  border: none;
+  cursor: pointer;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 24px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
+
+export const Price = styled.span`
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 22px;
+  color: #3C4044;
+  float: right;
+  margin-left: auto;
 `;
--- client/src/pages/cart/index.js
+++ client/src/pages/cart/index.js
@@ -4,8 +4,27 @@ import {
   CartPageContainer,
   Header,
   CartOptions,
+  ProductColumn,
+  ProductImg,
+  ButtonRow,
+  CouponButton,
+  SaveForLaterButton,
+  ProductList,
+  Product,
+  ProductName,
+  ProductDetails,
+  ExtraDetails,
+  Delete,
+  SubtotalDiv,
+  Total,
+  Checkout,
+  CheckoutButton,
+  Price,
 } from './cart.styles';
 import { Container } from '../../styles/global.style';
+import image from '../../assets/about-us/LinkerdInIcon.png'
+import Quantity from '../../components/quantity-field';
+import deleteIcon from '../../assets/icons/delete.svg';
 
 const Cart = () => {
 
@@ -19,7 +38,35 @@ const Cart = () => {
             <span>Save all for later</span>
             <span>Go to save for later list &gt;</span>
           </CartOptions>
+          <ProductList>
+            <Product>
+              <ProductColumn>
+                <ProductImg src={image} alt="product" />
+                <ButtonRow>
+                  <Quantity cta='cartPage' />
+                  <CouponButton>COUPON</CouponButton>
+                  <SaveForLaterButton>SAVE FOR LATER</SaveForLaterButton>
+                </ButtonRow>
+              </ProductColumn>
+              <ProductDetails>
+                <ProductName>Linked In</ProductName>
+                <Delete src={deleteIcon} alt="delete" />
+                <ExtraDetails>
+                  <li>Peach Black</li>
+                  <li>3 ounce (pack of 1)</li>
+                </ExtraDetails>
+                <Price>$120.00</Price>
+              </ProductDetails>
+            </Product>
+          </ProductList>
         </CartContainer>
+        <Checkout>
+          <SubtotalDiv>
+            <span>Subtotal: (not including tax or shipping)</span>
+            <Total>$51000</Total>
+            <CheckoutButton>GO TO CHECKOUT</CheckoutButton>
+          </SubtotalDiv>
+        </Checkout>
       </CartPageContainer>
     </Container>
   );
