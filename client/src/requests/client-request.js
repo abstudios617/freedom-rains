0550fd4f1f7240e730f8a00a5e9d645034d9963e
AlamTahmidul <mohammed.alam.217@gmail.com>
2022-10-14 12:56:23 -0400

    Clean up
    removed extraneous no response

--- client/src/requests/client-request.js
+++ client/src/requests/client-request.js
@@ -27,7 +27,6 @@ export const client = async (url = '', body = null, wildcards = null) => {
   }
 
   const response = await axios(options);
-  if (!response) return {statusCode: response};
   response.data.statusCode = response.status;
   return response.data;
 };
