6c88b190f2baf54190f8e1ad621ea25251069653
Ji-min Kim <kimjimin1226@gmail.com>
2022-10-17 17:33:39 -0400

    check if git commit works

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,8 @@ export const api = process.env.REACT_APP_API;
 
 export const imgServer = process.env.REACT_APP_IMG_URL;
 
-export const amplitudeKey = process.env.REACT_APP_AMP_KEY;
+//Had to set the key manually or else it wouldn't work. 
+export const amplitudeKey = '3d3d8925ed298021fdfb0bb4397aa8d8';
 
 export const printfulKey = process.env.REACT_APP_PRINTFUL_KEY;
 
