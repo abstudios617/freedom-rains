7a1fc94c2c8946dc63f99a893ac9c321dabaa583
Ji-min Kim <kimjimin1226@gmail.com>
2022-11-10 17:15:48 -0500

    erased comments

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,6 @@ export const api = process.env.REACT_APP_API;
 
 export const imgServer = process.env.REACT_APP_IMG_URL;
 
-//Had to set the key manually or else it wouldn't work. 
 export const amplitudeKey = process.env.REACT_APP_AMP_KEY;
 
 export const printfulKey = process.env.REACT_APP_PRINTFUL_KEY;
